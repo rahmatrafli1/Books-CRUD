768b31ae7fa562e800e297d12a729f0d9c519a6e
rahmatrafli1 <rahmatrafli1@gmail.com>
2023-11-06 11:24:59 +0700

    menambahkan getId dan Post pada controller books

--- backend/src/books/books.controller.ts
+++ backend/src/books/books.controller.ts
@@ -1,9 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('books')
 export class BooksController {
   @Get()
   hello() {
     return 'Hello World';
+  }
+
+  @Get('/:name')
+  helloid(@Param('name') name: string) {
+    return 'Hello ' + name;
+  }
+
+  @Post()
+  createBooks(@Body('name') name: string) {
+    return name;
   }
 }
